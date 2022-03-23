46598e508ea00e5cac5d48a40b85b386a047571b
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-03-23 12:44:09 -0500

    Todos con Rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import TodoDetails from './Componentes/TodoDetails/TodoDetails';
+import TodoForm from './Componentes/TodoForm/TodoForm';
+import TodoList from './Componentes/TodoList/TodoList';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const todosIniciales = [{
+    nombre : 'Aprender componentes de tipo clase.',
+    status : 'En progreso',
+    id : 123
+  },
+  {
+    nombre : 'Aprender eventos en React.',
+    status : 'En progreso',
+    id : 456
+  }];
+
+  const todoNuevoInicial = {
+    nombre : '',
+    status : '',
+    id : 0
+  };
+
+  const [nombre, setNombre] = useState( 'Alexander' );
+  const [apellido, setApellido] = useState( 'Martinez' );
+  const [edad, setEdad] = useState( 25 );
+  const [todos, setTodos] = useState( todosIniciales );
+  const [nuevoTodo, setNuevoTodo] = useState( todoNuevoInicial );
+
+  const cambiarDatos = () => {
+    setNombre( (nombrePrev) => 'Jose' );
+    setApellido( (apellidoPrev) => 'Angulo' );
+    setEdad( (edadPrev) => edadPrev + 1 );
+  }
+
+  const actualizarTodo = ( idTodo, statusNuevo ) => {
+    let todosActualizados = [...todos];
+    for( let i = 0; i < todosActualizados.length; i ++ ){
+      if( todosActualizados[i].id === idTodo ){
+        todosActualizados[i].status = statusNuevo;
+      }
+    }
+    setTodos( (todosPrev) => todosActualizados );
+  }
+
+  const agregarNuevoTodo = (event) => {
+    event.preventDefault();
+
+    setTodos( (todosPrev) => [...todosPrev, nuevoTodo] );
+    setNuevoTodo( (todoNuevoPrev) => todoNuevoInicial );
+  }
+
+  const actualizaCampoNuevoTodo = ( propiedad, valor ) => {
+    setNuevoTodo({
+        ...nuevoTodo,
+        [propiedad] : valor
+    });
+  }
+
+  return(
+    <BrowserRouter>
+        <h2>
+          Bienvenido de vuelta {nombre} {apellido}. Edad {edad}
+        </h2>
+        <ul className="navegacion">
+          <li className="opcion">
+            <Link to="/"> Dashboard </Link>
+          </li>
+          <li className="opcion">
+            <Link to="/todo/nuevo"> Agregar Todo </Link>
+          </li>
+          <li className="opcion">
+            <Link to="/todos"> Lista de todos </Link>
+          </li>
+        </ul>
+        <button onClick={cambiarDatos}>
+          Cambiar Datos
+        </button>
+        <Switch>
+          <Route path="/todo/nuevo" 
+               render={ (routeProps) => <TodoForm agregarNuevoTodo={agregarNuevoTodo}
+                                              nuevoTodo={nuevoTodo}
+                                              actualizaCampoNuevoTodo={actualizaCampoNuevoTodo}
+                                              {...routeProps}/>} />
+          <Route path="/todos" render={ (routeProps) => <TodoList todos={todos} 
+                                                          actualizarTodo={actualizarTodo}
+                                                          {...routeProps}/>} />
+          <Route path="/todo/:identificador" render={ (routeProps) => <TodoDetails {...routeProps} todos={todos} />} />
+        </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Componentes/Todo/Todo.js
@@ -0,0 +1,42 @@
+import './Todo.css';
+import { Link } from 'react-router-dom';
+
+function Todo( props ){
+    console.log( props );
+    return(
+        <div className='tarjeta_todo'>
+            <h3 className='titulo_todo'>
+                Pendiente: {props.todo.nombre}
+            </h3>
+            <p className='status_todo'>
+                Status: {props.todo.status}
+            </p>
+            
+            <div>
+                {
+                (props.todo.status !== 'Completo') ? 
+                    (<button onClick={() => props.actualizarTodo( props.todo.id, 'Completo' )}>
+                        Completar
+                    </button>) : "" 
+                }
+                {
+                (props.todo.status !== 'En progreso') ? 
+                    (<button onClick={() => props.actualizarTodo( props.todo.id, 'En progreso' )}>
+                        En progreso
+                    </button>) : ""
+                }
+                {
+                    (props.todo.status !== 'Cancelado') ?
+                    (<button onClick={() => props.actualizarTodo( props.todo.id, 'Cancelado' )}>
+                        Cancelar
+                    </button>) : ""
+                }
+                
+            </div>
+            <Link to={'/todo/' + props.todo.id}> Ver detalle </Link>
+            
+        </div>
+    );
+}
+
+export default Todo;
